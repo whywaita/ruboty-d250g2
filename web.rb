8a4b91a47f3a37623db0dac9398bde14c7332c50
whywaita <whywaita@whywrite.it>
2017-02-23 19:23:17 +0900

    port modify 8000 to 80

--- web.rb
+++ web.rb
@@ -4,7 +4,7 @@ module Ruboty
   module Handlers
     class Web < Base
       Thread.new do
-        WEBrick::HTTPServer.new(:DocumentRoot => "./public/", :Port => 8000).start
+        WEBrick::HTTPServer.new(:DocumentRoot => "./public/", :Port => 80).start
       end
     end
   end
